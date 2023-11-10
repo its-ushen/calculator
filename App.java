c96ce2ada2e7431d66b705a131bbb03ec06bfb19
ruqn <59122094+ruanodowd@users.noreply.github.com>
2023-11-10 11:36:46 +0000

    Skeleton Code

---
+++ App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) {
+        
+    }
+    public void askQuestion(){
+
+    }
+    public double add(double a, double b){
+
+    }
+    public double multiply(double a, double b){
+
+    }
+    public double divide(double a, double b){
+
+    }
+    public double subtract(double a, double b){
+
+    }
+}
